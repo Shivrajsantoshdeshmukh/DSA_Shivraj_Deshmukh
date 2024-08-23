eb72b2c04a271f05824c1337f2905bb2faf5de27
Shivrajsantoshdeshmukh <163736106+Shivrajsantoshdeshmukh@users.noreply.github.com>
2024-08-24 01:05:59 +0530

    01:05 AM - 24/08/2024

---
+++ LEC3/2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    a=10;
+    cout<<a<<endl;
+    char c='a';
+    cout<<c<<endl;
+    float f=10.5;
+    cout<<f<<endl;
+    double d=3.15555;
+    cout<<d<<endl;
+    bool b=10;
+    cout<<b<<endl;
+}
